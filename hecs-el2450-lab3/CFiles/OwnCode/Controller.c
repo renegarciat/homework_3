df57cdc2b8c403f0487482e041d05c795cc7989e
René G. <93566009+renegarciat@users.noreply.github.com>
2026-02-23 15:48:38 +0100

    Up to task 8.

--- hecs-el2450-lab3/CFiles/OwnCode/Controller.c
+++ hecs-el2450-lab3/CFiles/OwnCode/Controller.c
@@ -15,23 +15,22 @@
 // Uncomment to do only a rotation control.
 // Task 6. Implement a controller that stabilize theta in theta_R.
 //  We can derive the new angular speed
-// omega = K_PSI_1*(theta_R-theta);
-// // Convert angular speed to  left and right, given that v = 0.
-// left = -omega*L_TRUE/(2*R_TRUE);
-// right = omega*L_TRUE/(2*R_TRUE);
+omega = K_PSI_1*(theta_R-theta);
+// Convert angular speed to  left and right, given that v = 0.
+left = -omega/2;
+right = omega/2;
 
 // Uncomment to do only a pose control.
 // Task 7. Maintain the robot in the current position while rotating.
 // calculate the velocity vector
-v_c[0] = cos(theta*(M_PI/180.0)); // Desired velocity in the x direction in cm/s.
-v_c[1] = sin(theta*(M_PI/180.0)); // Desired velocity in the y direction in cm/s.
+v_c[0] = cos(theta*(M_PI/180.0)); // Desired velocity in the x direction in m/s.
+v_c[1] = sin(theta*(M_PI/180.0)); // Desired velocity in the y direction in m/s.
 // Calculate the error in position
-delta_0[0] = x0-x; // Position error in the x direction in cm.
-delta_0[1] = y0-y; // Position error in the y direction in cm.
+delta_0[0] = (x0-x) / 100.0; // Position error in the x direction in m.
+delta_0[1] = (y0-y) / 100.0; // Position error in the y direction in m.
 // Calculate the inner product of the velocity vector and the position error
 d_0 = v_c[0]*delta_0[0] + v_c[1]*delta_0[1];
 // Calculate the desired translational velocity
-v = K_OMEGA_1*d_0;
-// Assuming omega = 0
-left = v;
-right = v;
+v = K_OMEGA_1*d_0; // [m/s]
+left = v*100; // We dont know the units of left and right, but assume cm/s.
+right = v*100;
